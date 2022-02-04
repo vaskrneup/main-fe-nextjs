5fd0a6bc7331ea692cc25142b08e5475de71d1a1
vaskrneup <bhaskar.neupane.58@gmail.com>
2022-02-04 09:54:19 +0000

    Deploying to gh-pages from @ vaskrneup/main-fe-nextjs@c5b947c997b9d7ee41863b460e9f6ef62c655d12 🚀

---
+++ _next/static/nypAPK6jJLMKwng2etYFA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-bd638497dbb8bf67.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/test":["static/chunks/pages/test-cb5362094cf4c151.js"],sortedPages:["/","/_app","/_error","/test"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
