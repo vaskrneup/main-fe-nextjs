4ab16ac915b7ca5e335eae4b5bca8aa24b2f8ae6
vaskrneup <39903236+vaskrneup@users.noreply.github.com>
2022-02-08 08:49:42 +0000

    Deploying to gh-pages from @ vaskrneup/vaskrneup.github.io@36be56fab2a1e58c33e15f9c262741f52122aba9 🚀

--- _next/static/PWi76UeIpTH-CXVOfUo0m/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/134bd1b375c59904.css","static/chunks/pages/index-4f9f1178ad75c005.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/WhYNBwdeMxQR1xH7t5K2o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/772b5e0e15cf6e51.css","static/chunks/pages/index-7eabbcf0711b9383.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
