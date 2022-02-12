683465d3ad838adf00b5d87873c15d16775bafea
vaskrneup <39903236+vaskrneup@users.noreply.github.com>
2022-02-12 13:15:42 +0000

    Deploying to gh-pages from @ vaskrneup/main-fe-nextjs@ed594db8bfa3b4e361d3bf062fc34af4b89d261f 🚀

--- _next/static/lBw4lFOPXI6lxI0hnfEFh/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout-me","\u002Fblogs","\u002F","\u002Fprojects","\u002Futils\u002Fsitemap-generator","\u002Fblogs\u002F[blogId]","\u002Fprojects\u002F[projectId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
